1c679e045454964b7d061efa0028233a112320f1
David Rodriguez <dr507498@gmail.com>
2025-10-21 15:54:01 -0400

    Chapter 10 addition and Chapter 12!

---
+++ 010_functions/local_varaibles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+example() {
+    local var1=$1
+    local var2=$2
+
+    echo $var1 $var2
+}
+
+example "Hello" "World!"
---
+++ 012_scheduling_jobs/example.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hello World!"
+
+# at 12:47 -f ./c012_scheduling_jobs/example.sh
